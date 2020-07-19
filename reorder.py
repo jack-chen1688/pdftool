7bacedc52835d62cbc2c1067fab33f1c7c020839
Xuehua Chen <xuehua@gmail.com>
2020-07-18 20:57:12 -0700

    Minor changes.

--- reorder.py
+++ reorder.py
@@ -2,19 +2,24 @@ import sys
 from PyPDF2 import PdfFileWriter, PdfFileReader
 
 
-output_pdf = PdfFileWriter()
-input_name = sys.argv[1]
-output_name = sys.argv[2]
+def main():        
+    output_pdf = PdfFileWriter()
+    input_name = sys.argv[1]
+    output_name = sys.argv[2]
 
-with open(input_name, 'rb') as readfile:
-    input_pdf = PdfFileReader(readfile)
-    total_pages = input_pdf.getNumPages()
-    
-    # reverse file orders and also rotate counter-clockwise each page 90 degrees. 
-    for page in range(total_pages - 1, -1, -1):
-        pdf_page = input_pdf.getPage(page)
-        pdf_page.rotateClockwise(270)
-        output_pdf.addPage(pdf_page)
-    
-    with open(output_name, 'wb') as writefile:
-        output_pdf.write(writefile)
+    with open(input_name, 'rb') as readfile:
+        input_pdf = PdfFileReader(readfile)
+        total_pages = input_pdf.getNumPages()
+        
+        # reverse file orders and also rotate counter-clockwise each page 90 degrees. 
+        for page in range(total_pages - 1, -1, -1):
+            pdf_page = input_pdf.getPage(page)
+            pdf_page.rotateClockwise(270)
+            output_pdf.addPage(pdf_page)
+        
+        with open(output_name, 'wb') as writefile:
+            output_pdf.write(writefile)
+
+
+if __name__ == "__main__":
+    main()
